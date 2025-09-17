1135669500fe7bdc694de7f8ff59ea3eb4d09808
zack <zzhang_xz@163.com>
2025-09-17 15:56:06 +0800

    feat(cert): add test cert

--- utilr/jwt_test.go
+++ utilr/jwt_test.go
@@ -21,7 +21,7 @@ func TestCreateToken(t *testing.T) {
 		Email:    "string",
 	}
 
-	jwt := NewJWT("../source/cert/private_key.pem")
+	jwt := NewJWT("./private_key.pem")
 
 	token, err := jwt.CreateToken(jwt.CreateClaims(bc, "1d", "70d", "TEST"))
 	if err != nil {
@@ -31,8 +31,8 @@ func TestCreateToken(t *testing.T) {
 }
 
 func TestParser(t *testing.T) {
-	token := "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJJRCI6MSwiS2V5SUQiOiJzYXMiLCJBdWRpZW5jZSI6WyIvb25laWQvYWFjcy8qIiwiL29uZWlkL2FhY3Mvd3BwLWFkbWluIiwiL29uZWlkL2FhY3Mvd3BwLWFkbWluL2EiXSwiU3ViamVjdCI6IndwcC1hZG1pbiIsIlZhbHVlIjpudWxsLCJUeXBlIjoxLCJOaWNrbmFtZSI6IuiMg-engOiLsSIsIkdlbmRlciI6IiIsIkNvdmVyIjoiaHR0cHM6Ly9xbXBsdXNpbWcuaGVucm9uZ3lpLnRvcC9ndmFfaGVhZGVyLmpwZyIsIlBob25lIjoiMTg2MDU1NDg4ODQiLCJFbWFpbCI6IiIsIkJ1ZmZlclRpbWUiOjg2NDAwLCJpc3MiOiJPTkVJRF9BQUNTIiwic3ViIjoid3BwLWFkbWluIiwiYXVkIjpbIi9vbmVpZC9hYWNzLyoiLCIvb25laWQvYWFjcy93cHAtYWRtaW4iLCIvb25laWQvYWFjcy93cHAtYWRtaW4vYSJdLCJleHAiOjE3MTg3MjEyNjMsIm5iZiI6MTcxMjY3MzI2M30.dNzrIlWf_FcoxCGLZW9f0qDt8HSqU1_xhzUnD48DpUC6ccBmvT9V1dNmLxqU6OOZYNccyadro71GbJuVTHmuX6f7FrjEkxr3xb4gcFuAie9vSBqDa5RxZY9WaqgG5GGtxozQOZn2DW6-LVudn1s_VLe5ieqvUCy5FZFPNXEw7aOWDKRjtsW-ZyWMrRZ4jvX2p_KKvg34IdF1cg_PSNM1UIXvJsCV8S-pIJZ_a3qjIaCuk-foEaJbGH_yCWkg7e_nWpeArhSKDs4cEfXdm2D7JMW_SLX4u86rRotXv_b-UaTUd7F1UaGFteap4tW9UFP7WhuM5qOnpnCBpbs7HyMKxQ"
-	jwt, _ := utilo.GetParser("../source/cert/public_key.pem")
+	token := "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJJRCI6MSwiS2V5SUQiOiJzdHJpbmciLCJBdWRpZW5jZSI6WyJzdHJpbmciXSwiU3ViamVjdCI6IiIsIlZhbHVlIjpudWxsLCJUeXBlIjoxLCJOaWNrbmFtZSI6InN0cmluZyIsIkdlbmRlciI6InN0cmluZyIsIkNvdmVyIjoic3RyaW5nIiwiUGhvbmUiOiJzdHJpbmciLCJFbWFpbCI6InN0cmluZyIsIkJ1ZmZlclRpbWUiOjg2NDAwLCJpc3MiOiJURVNUIiwiYXVkIjpbInN0cmluZyJdLCJleHAiOjE3NjQxNDM2OTYsIm5iZiI6MTc1ODA5NTY5Nn0.Vv8MszfQct_eBK-SQ_9XplkVCzO3hzF87OWOWZ-UbwHCxFltXTvPhajG3QBKj3xqIBsRSCa8PqVEWguGKdsJnQHdrk5r_X4CQfzqQ_HA8uIQG4OL9w6cShlRBe4_1tUGXqgYLclivV7o3kKgb6UihWfzVO9sSnptLBusSmAteT3CwUx7N2aR_oMG3_31yeGqbmDgn2clX9uy-aCkx2YWlPpKxtxzZHmpqxIAtLSzpAArSvo_dh5taqQtYn62QMdK49S6rlOlfyQb30E12AC6Ue3iTxl0BMyCsK3AYQqkiTB7uyfrXQW_JxxOmQZUHv9z0gmG1ugHx3mxoWz3-O24eg"
+	jwt, _ := utilo.GetParser("./public_key.pem")
 	parseToken, err := jwt.ParseToken(token)
 	if err != nil {
 		return
@@ -40,4 +40,8 @@ func TestParser(t *testing.T) {
 
 	fmt.Println(parseToken)
 
+}
+
+func TestGenePem(t *testing.T) {
+	utilo.GenRsaCertPair()
 }
