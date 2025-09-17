282347c04775b4c8846cdc43062acf02c4f6df61
zack <zzhang_xz@163.com>
2025-09-17 16:55:46 +0800

    feat(common): add v2 types with int64 support

--- request/common.go
+++ request/common.go
@@ -7,22 +7,41 @@ type PageInfo struct {
 	Keyword  string `json:"keyword" form:"keyword"`   //关键字
 }
 
+type PageInfoV2 struct {
+	Page     int64  `json:"page" form:"page"`         // 页码
+	PageSize int64  `json:"pageSize" form:"pageSize"` // 每页大小
+	Keyword  string `json:"keyword" form:"keyword"`   //关键字
+}
+
 // GetById Find by id structure
 type GetById struct {
 	Id int `json:"id" form:"id"` // 主键ID
 }
+type GetByIdV2 struct {
+	Id int64 `json:"id" form:"id"` // 主键ID
+}
 
 func (r *GetById) Uint() int {
-	return int(r.Id)
+	return r.Id
+}
+func (r *GetByIdV2) Uint() int64 {
+	return r.Id
 }
 
 type IdsReq struct {
 	Ids []int `json:"ids" form:"ids" binding:"required"`
 }
 
+type IdsReqV2 struct {
+	Ids []int64 `json:"ids" form:"ids" binding:"required"`
+}
+
 // GetAuthorityId Get role by id structure
 type GetAuthorityId struct {
 	AuthorityId int `json:"authorityId" form:"authorityId"` // 角色ID
+}
+type GetAuthorityIdV2 struct {
+	AuthorityId int64 `json:"authorityId" form:"authorityId"` // 角色ID
 }
 
 type Empty struct{}
--- response/common.go
+++ response/common.go
@@ -6,3 +6,10 @@ type PageResult struct {
 	Page     int         `json:"page"`
 	PageSize int         `json:"pageSize"`
 }
+
+type PageResultV2 struct {
+	List     interface{} `json:"list"`
+	Total    int64       `json:"total"`
+	Page     int64       `json:"page"`
+	PageSize int64       `json:"pageSize"`
+}
